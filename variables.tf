96617c53568d5546fc7df7753f3e12d9a3b7a9ca
aadityapalsagwan <aditya.pal40443@gmail.com>
2026-04-03 17:47:14 +0530

    EC2 module added

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "ami" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "name" {
+  description = "EC2 Name"
+  type        = string
+}
